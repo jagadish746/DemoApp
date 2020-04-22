fde7d4d4a1d95ff6f272959167aac7aa8d15853f
jagadish746 <61132492+jagadish746@users.noreply.github.com>
2020-04-22 12:34:22 +0530

    Add files via upload

---
+++ StudentRest/src/main/java/com/capg/springboot/StudentRestApplication.java
@@ -0,0 +1,13 @@
+package com.capg.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentRestApplication.class, args);
+	}
+
+}
